b7054a1d964e2249f49ec3c69b79df44a7f3595d
A.Mahmoud <a.mahmoud@gju.edu.jo>
2023-06-08 10:38:02 +0300

    ss

---
+++ src/Final/Employee.java
@@ -0,0 +1,33 @@
+package Final;
+
+public class Employee {
+    private int ID;
+    private int Salary;
+    private int Rank;
+
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(int salary) {
+        Salary = salary;
+    }
+
+    public int getRank() {
+        return Rank;
+    }
+
+    public void setRank(int rank) {
+        Rank = rank;
+    }
+}
