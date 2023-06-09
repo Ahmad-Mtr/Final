b7fd8f507d180936cd81a64bd8d0bbf88efa92ca
AhmadMtr <ahmadmtr@protonmail.com>
2023-06-09 20:37:10 +0300

    Final v3

--- src/Final_v3/Class1.java
+++ src/Final_v3/Class1.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Class1 implements ActionListener {
     private JMenuBar menuBar;
@@ -18,7 +19,7 @@ public class Class1 implements ActionListener {
     private JButton GUI1_SubmitButton, GUI2_Butt_FtoC, GUI2_Butt_CtoF, Reset;
     private FileReader UN_Read, Pass_Read;
     private BufferedReader UN_BuffRead, Pass_BuffRead;
-    private ArrayList<String> Usernames, Passwords;
+    private String[] Usernames, Passwords;
     int NumofLines = 0;
     boolean _isInfo = false;
 
@@ -50,18 +51,21 @@ public class Class1 implements ActionListener {
     public static void main(String[] arr) {
 
         Class1 x = new Class1();
-        x.m2();
+        //x.m2();
         x.m1();
     }
+
     public boolean checkpass(String UNPath, String PassPath) throws IOException {
         try {
             UN_Read = new FileReader(UNPath);
+            System.out.println("1");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
         UN_BuffRead = new BufferedReader(UN_Read);
         try {
             Pass_Read = new FileReader(PassPath);
+            System.out.println("2");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
@@ -70,29 +74,40 @@ public class Class1 implements ActionListener {
         boolean _isPass = false;
         //
         String temp;
-        Usernames = new ArrayList<String>();
-        Passwords = new ArrayList<String>();
+        Usernames = new String[16];
+        Passwords = new String[16];
 
         for (int i = 0; (temp = UN_BuffRead.readLine()) != null; i++) {
+            System.out.println("3");
             temp = UN_BuffRead.readLine();
             NumofLines++;
-            Usernames.add(temp);
+            System.out.println("\t" + NumofLines);
+            Usernames[i] = temp;
+            System.out.println("\t\t\t" + Usernames[i]);
         }
         for (int i = 0; (temp = Pass_BuffRead.readLine()) != null; i++) {
+            System.out.println("4");
             temp = Pass_BuffRead.readLine();
             //NumofLines++;
-            Passwords.add(temp);
+            Passwords[i] = temp;
         }
         //
 
         int i;
-        for (i = 0; i < NumofLines; i++) {
-            if (Usernames.get(i).equals(GUI1_TF_UserName1.getText())) {
+        String x = GUI1_TF_UserName1.getText();
+        String y = GUI1_TF_Password2.getText();
+        System.out.println("zzzzzzzzzzzzzzzzzzz");
+        for (i = 0; i < 17; i++) {
+            String z = Usernames[i];
+            System.out.println(z);
+            if (z == x) {
+                System.out.println("5A");
                 _isUN = true;
             }
-            if (_isUN == true) break;
+            if (_isUN) break;
         }
-        if (Passwords.get(i).equals(GUI1_TF_Password2.getText())) {
+        if (Objects.equals(Passwords[i], y)) {
+            System.out.println("6");
             _isPass = true;
         }
         return _isPass;
@@ -101,6 +116,7 @@ public class Class1 implements ActionListener {
     public void read(String UNPath, String PassPath) throws IOException {
 
         _isInfo = checkpass(UNPath, PassPath);
+        //_isInfo =true;
         if (_isInfo) {
             m2();
         } else {
@@ -172,7 +188,7 @@ public class Class1 implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == GUI1_SubmitButton) {
             try {
-                read("Usernames.txt", "Passwords.txt");
+                read("C:\\Users\\YOUSEF MAHMOUD\\IdeaProjects\\Final\\src\\Final_v3\\Usernames.txt", "C:\\Users\\YOUSEF MAHMOUD\\IdeaProjects\\Final\\src\\Final_v3\\Passwords.txt");
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
@@ -185,25 +201,25 @@ public class Class1 implements ActionListener {
         }
         if (e.getSource() == GUI2_Butt_FtoC) {//to Celsius
             String Fah = GUI2_TF_Fahren.getText();
-            double Fahrenheit = +  Double.parseDouble(Fah);
-            double Celsius = (Fahrenheit - 32) * (5 / 9);
+            double Fahrenheit = Double.parseDouble(Fah);
+            double Celsius = (Fahrenheit - 32) * ((double) 5 / 9);
             String x = String.valueOf(Celsius);
             GUI2_TF_Cels.setText(x);
         }
         if (e.getSource() == GUI2_Butt_CtoF) {// tp Fahrenheit
             String Cel = GUI2_TF_Cels.getText();
             double Celsius = Double.parseDouble(Cel);
-            double Fahrenheit = (  Celsius * (9/ 5)) + 32;
+            double Fahrenheit = (Celsius * ((double) 9 / 5)) + 32;
             String x = String.valueOf(Fahrenheit);
             GUI2_TF_Fahren.setText(x);
         }
-        if (e.getSource() == Reset){
+        if (e.getSource() == Reset) {
             System.out.println(GUI2_TF_Cels.getText());
             System.out.println(GUI2_TF_Fahren.getText());
             GUI2_TF_Cels.setText("0.00");
             GUI2_TF_Fahren.setText("0.00");
         }
-        if (e.getSource() == ClearData){
+        if (e.getSource() == ClearData) {
             FileWriter FW = null;
             try {
                 FW = new FileWriter("Usernames.txt");
@@ -239,10 +255,10 @@ public class Class1 implements ActionListener {
                 throw new RuntimeException(ex);
             }
         }
-        if (e.getSource() == ShowUsers){
+        if (e.getSource() == ShowUsers) {
 
         }
-        if (e.getSource() == CountUsers){
+        if (e.getSource() == CountUsers) {
             JOptionPane.showMessageDialog(GUI2_Frame, NumofLines);
         }
     }
